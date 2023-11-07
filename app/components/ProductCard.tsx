4c78e374b58c0a5c97d7b2d9664de5376cae8389
Tong Tang <pichip22@hotmail.com>
2023-11-07 10:53:02 -0500

    testing some pages and prisma

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ProductCard = () => {
+  return (
+    <div>ProductCard
+    <AddToCart />
+
+    </div>
+  )
+}
+
+export default ProductCard
---
+++ app/users/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const newUsersPage = () => {
+  return (
+    <div>newUsersPage</div>
+  )
+}
+
+export default newUsersPage
