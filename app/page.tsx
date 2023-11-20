c2f3abc64f71d5c131129a697822043eb5258908
Tong Tang <pichip22@hotmail.com>
2023-11-20 16:54:24 -0500

    fix import

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import NavBar from 'app/components/NavBar'
+import NavBar from '../app/components/NavBar'
 export default function Home() {
   return (
     <main>
       <NavBar></NavBar>
-      <h1 className='main_header'>Yealt's Home</h1>      
+      <h1 className='main_header'>Yealt`s Home</h1>      
     </main>
   )
 }
