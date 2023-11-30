68eb39cdc1fac650df6b4ffe22ac92579e5f73d9
yeah1tnt <pichip22@hotmail.com>
2023-11-30 16:34:11 -0500

    added css for navigation

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,12 +1,18 @@
-import react from "react";
+'use client'
+import react, {useState} from "react";
 import Link from "next/link";
 
 function NavBar() {
+    const [click, setClick] = useState(false)
+    function onClick() {
+        setClick(!click)
+    }
+
     return (
         <div className ='nav'>
-            <Link href='/'>Home</Link> <></>
-            <Link href='/youtube'>Youtube</Link> <></>
-            <Link href='/about'>About</Link>
+            <div className ={`navElement ${click ? '':''}`} onClick={onClick}><Link href='/'>Home</Link></div>
+            <div className ='navElement'><Link href='/youtube'>Youtube</Link></div>
+            <div className ='navElement'><Link href='/about'>About</Link></div>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import NavBar from '../app/components/NavBar'
 export default function Home() {
   return (
-    <main>
+    <div className='flex h-screen flex-col md:flex-row justify-center'>
       <NavBar></NavBar>
-      <h1 className='main_header'>Yealt`s Home</h1>      
-    </main>
+      <h1 className='main_header'>Yealt`s Home</h1>   
+     </div>   
   )
 }
